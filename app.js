5fc567c990bec9149ebb65dc918e6f7125b8f49f
Sandeep Wadhwa <sandeep.sajiru@gmail.com>
2015-08-20 23:37:13 +0530

    Added a route for thought of the day api

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ app.get('/', function(req, res){
 	res.send('Welcome to RESTful');
 });
 
+var thoughtsRouter = express.Router();
+thoughtsRouter.route('/thoughts')
+			  .get(function(req, res){
+				  res.json({'thought':'What a lovely weather',
+				  'date':'2015/08/21'});
+			  });
+app.use('/api', thoughtsRouter);
+
 app.listen(3000, function()
 {
 	console.log('Server started on http://localhost:3000');
